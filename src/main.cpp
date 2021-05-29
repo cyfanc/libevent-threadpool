721a593382255a66c61fffd440190cb1b2864ac7
chen <772535307@qq.com>
2021-05-29 17:23:16 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -11,9 +11,9 @@ int main(int argc, char** argv)
 {
 	gettimeofday(&g_st, NULL);
 
-	Server* server = new Server("0.0.0.0",8888,"172.19.3.58",3306);
+	Server* server = new Server("0.0.0.0",8888,"172.198.1.25",3306);
 
-    server->run();
+    	server->run();
 
-    return 0;
+   	return 0;
 }
